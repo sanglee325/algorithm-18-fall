7dd1c8a8fa60a07b9dcb66d26e62b726564b5e18
sanglee <pksll325@gmail.com>
2018-11-18 09:22:12 -0800

    indent revised

--- HW1/caserand.c
+++ HW1/caserand.c
@@ -2,19 +2,19 @@
 #include <time.h>
 
 int main(){
-      int num, N = 10000;
-        FILE *fp = fopen("input00012.txt", "w");
+    int num, N = 10000;
+    FILE *fp = fopen("input00012.txt", "w");
 
 
-          fprintf(fp, "%d ", N);
-            srand((unsigned)time(NULL));
+    fprintf(fp, "%d ", N);
+    srand((unsigned)time(NULL));
 
-              for(int i = 0; i < N; i++){
-                        num = rand() % N - (N/2);
-                              fprintf(fp, "%d ", num);
-                                }
+    for(int i = 0; i < N; i++){
+        num = rand() % N - (N/2);
+        fprintf(fp, "%d ", num);
+    }
 
-                fclose(fp);
+    fclose(fp);
 
-                  return 0;
+    return 0;
 }
