e9c7440769871b2d2f5073eaeabdabf27cb83803
sanglee <pksll325@gmail.com>
2018-12-09 18:30:02 -0800

    compressed? #5

--- HW3/ppHuff.cpp
+++ HW3/ppHuff.cpp
@@ -12,7 +12,7 @@ typedef struct _node{
     char data;
     unsigned int b;
     bool leaf;
-    int max, child;
+    int max, child, len;
     struct _node* left;
     struct _node* right;
 }NODE;
@@ -33,6 +33,7 @@ void Compress();
 void createHeap();
 void encode();
 void postOrder(NODE *node, NODE *parent, NODE *target);
+void createZipFile();
 
 void Decompress();
 int type;
@@ -71,7 +72,7 @@ void Compress(){
             Data[0].data = tmp;
             Data[0].freq = Data[0].max = 1;
             Data[0].leaf = true;
-            Data[0].child = 1;
+            Data[0].child = Data[0].len = 1;
             type++;
         }
         else{
@@ -97,7 +98,7 @@ void Compress(){
                     Data[type - 1].data = tmp;
                     Data[type - 1].freq = Data[type - 1].max = 1;
                     Data[type - 1].leaf = true;
-                    Data[type - 1].child = 1;
+                    Data[type - 1].child = Data[type - 1].len = 1;
                 }
             }
         }
@@ -111,10 +112,18 @@ void Compress(){
     free(Data);
     encode();
 
-    /*
-    for(i = 0; i < type; i++)
-      printf("Data[%d]: %c, freq: %d\n", i, Data[i].data, Data[i].freq);
-      */
+    createZipFile();
+    for(i = 0; i < type; i++){
+      printf("Data[%d]: %c, freq: %d, len: %d\n", i, Data[i].data, Data[i].freq, Data[i].len);
+        //printf("Data[%d]\ndata: %c, freq: %d\n", i, Data[i].data, Data[i].freq);
+        //printf("bit: ");
+       // printf("%d\n", Data[i].b);
+        for(int j = 31; j >= 0; --j){ 
+          printf("%d", (Data[i].b >> j)&1);
+          if(j % 4 == 0) printf(" ");
+        }
+        printf("\n");
+    }
     //need to make a trreeeeeeeeeee
 }
 
@@ -152,7 +161,9 @@ void createHeap(){
         newNode->max = tmp1->max < tmp2->max ? tmp2->max : tmp1->max; 
         newNode->child = tmp1->child + tmp2->child;
         newNode->leaf = false;
-        
+       
+        tmp1->len = tmp2->len = newNode->len = 1;
+
         if(tmp1->freq < tmp2->freq){
             newNode->left = tmp1;
             newNode->right = tmp2;
@@ -195,7 +206,7 @@ void encode(){
 
     idx = 0;
     postOrder(tmp, parent, target);
-    for(int i = 0; i < type; i++){
+/*    for(int i = 0; i < type; i++){
         //printf("Data[%d]\ndata: %c, freq: %d\n", i, Data[i].data, Data[i].freq);
         //printf("bit: ");
        // printf("%d\n", Data[i].b);
@@ -204,17 +215,19 @@ void encode(){
           if(j % 4 == 0) printf(" ");
         }
         printf("\n");
-    }
+    }*/
 }
 
 void postOrder(NODE *node, NODE *parent, NODE *target){
     if(node->left != NULL){
+        node->left->len = node->len + 1;
         node->left->b = node->b;
         node->left->b <<= 1;
         parent = node;
         postOrder(node->left, node, target);
     }
     if(node->right != NULL){
+        node->right->len = node->len + 1;
         node->right->b = node->b;
         node->right->b <<= 1;
         node->right->b |= 1;
@@ -226,8 +239,73 @@ void postOrder(NODE *node, NODE *parent, NODE *target){
         Data[idx].b = node->b;
         Data[idx].freq = node->freq;
         Data[idx].leaf = true;
+        Data[idx].len = --(node->len);
+        Data[idx].b = node->b;
         idx++;
     }
+}
+void createZipFile(){
+    FILE *in, *out;
+    int idx, end, len[2], sum;
+    char c;
+    unsigned int buf[2], temp, OFbuf = 0;
+    int buf_idx = 0;
+    bool full = false;
+
+    for(int i = 0; i < 2; i++){
+        buf[i] = 0;
+        len[i] = 0;
+    }
+    in = fopen(Filename, "r");
+    strcat(Filename, ".cc");
+    out = fopen(Filename, "wb");
+    while(1){
+        end = fscanf(in, "%c", &c);
+        if(end == EOF) break;
+        for(idx = 0; idx < type; idx++){
+            if(Data[idx].data == c){
+                OFbuf = temp = Data[idx].b; 
+                break;
+            }
+        }
+        len[buf_idx] += Data[idx].len;
+        if(len[buf_idx] > 32){
+            temp >>= len[buf_idx] - 32;
+            buf[buf_idx] |= temp;
+            if(buf_idx == 0){
+                len[1] = len[0] - 32;
+                OFbuf <<= 32 - len[1];
+                buf[1] |= OFbuf;
+            }
+            if(buf_idx == 1){
+                len[0] = len[1] - 32;
+                OFbuf <<= 32 - len[0];
+                buf[0] |= OFbuf;
+            }
+            full = true;
+        }
+        else{
+            temp <<= 32 - len[buf_idx];
+            buf[buf_idx] |= temp;
+        }
+        if(full == true){
+            fwrite(&buf[buf_idx], sizeof(unsigned int), 1, out);
+            buf[buf_idx] = 0;
+            len[buf_idx] = 0;
+            if(buf_idx == 1){
+                buf_idx = 0;
+            }
+            if(buf_idx == 0){
+                buf_idx = 1;
+            }
+            full = false;
+        }
+        OFbuf = temp = 0;
+    }
+            
+    fclose(in);
+    fclose(out);
+
 }
 
 void Decompress(){
